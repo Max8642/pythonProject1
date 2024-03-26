8ccc815bdd17dcb446d06e542d512107d2a73472
Max8642 <memasri101@gmail.com>
2024-03-25 21:17:33 -0400

    Test with scraped data

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    #return render_template('index.html')  # this line only displays files in the templates sub directory.  To display from other directories, see below
-   return flask.send_from_directory(".", path="index2.html")
+   return flask.send_from_directory(".", path="index.html")
 
 
 
